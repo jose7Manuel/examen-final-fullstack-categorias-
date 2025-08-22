50f3e1f3bbb5046150cc8879d0dfc8b67ede3770
jose7Manuel <jose.gomez@uab.edu.bo>
2025-08-21 22:43:47 -0400

    actualizacion de archivos

---
+++ backend/app/Http/Controllers/CategoryController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+   use App\Models\Category;
+use Illuminate\Http\Request;
+
+public function store(Request $request)
+{
+    $category = Category::create($request->all());
+    return response()->json($category, 201);
+}
+
+public function index()
+{
+    $categories = Category::all();
+    return response()->json($categories);
+}
+
+}
---
+++ backend/routes/api.php
@@ -0,0 +1,3 @@
+<?php
+use App\Models\Category;
+use Illuminate\Http\Request;
